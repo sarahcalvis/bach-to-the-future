5fa5e67a1c534a62c891beb286cbe5c26679efcd
sarahcalvis <sarahcalvis@gmail.com>
2019-04-09 19:53:43 -0400

    hahahahahahahaha

---
+++ RoboCalML/src/hilogan/Main.java
@@ -0,0 +1,15 @@
+package hilogan;
+
+public class Main {
+	public static void main(String[] args) {
+		ourFirstResolution();
+		printLogan();
+	}
+	static void ourFirstResolution() {
+		System.out.println("Don't leave me. I really like you.");
+	}
+	public static void printLogan(){
+		System.out.println("the best boyfriend");
+	}
+
+}
